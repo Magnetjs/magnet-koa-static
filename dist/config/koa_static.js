bc45a6f3b2fe2c4a36952da53994fc2e859de21e
Kieve Chua <kievechua@gmail.com>
2017-03-17 12:25:18 +0800

    Compile config

--- dist/config/koa_static.js
+++ dist/config/koa_static.js
@@ -1,11 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // https://github.com/koajs/static#options
-export default {
-  directory: 'static',
-
-  // maxage: '',
-  // hidden: '',
-  // index: '',
-  // defer: '',
-  // gzip: '',
-  // extensions: ''
-}
+exports.default = {
+    directory: 'static',
+};
+//# sourceMappingURL=koa_static.js.map
